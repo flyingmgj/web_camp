896db7ca68a5ae4cd3461c044ad10730bf4a4b59
magee <18219349847@163.com>
2019-03-31 15:02:54 +0800

    this is my first work

---
+++ firstweek/代码和头文件/DuLinkedList.c
@@ -0,0 +1,84 @@
+#include "DuLinkedList.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+/**************************************************************
+*	Prototype realization Section
+**************************************************************/
+
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status InitList_DuL(DuLinkedList *L);
+
+/**
+ *  @name        : void DestroyList_DuL(DuLinkedList *L)
+ *	@description : destroy a linked list
+ *	@param		 : L(the head node)
+ *	@return		 : status
+ *  @notice      : None
+ */
+void DestroyList_DuL(DuLinkedList *L);
+
+/**
+ *  @name        : Status InsertBeforeList_DuL(DuLNode *p, LNode *q)
+ *	@description : insert node q before node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status InsertBeforeList_DuL(DuLNode *p, DuLNode *q);
+
+/**
+ *  @name        : Status InsertAfterList_DuL(DuLNode *p, DuLNode *q)
+ *	@description : insert node q after node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status InsertAfterList_DuL(DuLNode *p, DuLNode *q);
+
+/**
+ *  @name        : Status DeleteList_DuL(DuLNode *p, ElemType *e)
+ *	@description : delete the first node after the node p and assign its value to e
+ *	@param		 : p, e
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status DeleteList_DuL(DuLNode *p, ElemType *e);
+
+/**
+ *  @name        : void TraverseList_DuL(DuLinkedList L, void (*visit)(ElemType e))
+ *	@description : traverse the linked list and call the funtion visit
+ *	@param		 : L(the head node), visit
+ *	@return		 : Status
+ *  @notice      : None
+ */
+void TraverseList_DuL(DuLinkedList L, void(*visit)(ElemType e));
+
+
+//建立双链表
+void create(DuLNode *cre, int *arr, int n);
+
+//建立打印函数
+void print(ElemType e);
+
+//插入
+void insert(DuLNode *list);
+
+//找到最后一个节点
+DuLNode *final(DuLNode *find);
+
+//菜单
+void menu();
+
+//封装删除函数
+void delete(DuLNode *del);
+
+//封装更新函数
+void update(DuLNode *ln);
---
+++ firstweek/代码和头文件/头文件/DuLinkedList.h
@@ -0,0 +1,129 @@
+/***************************************************************************************
+ *	File Name				:	duLinkedList.h
+ *	CopyRight				:	2019 QG Studio
+ *	SYSTEM					:   win10
+ *	Create Data				:	2019.3.20
+ *	Author/Corportation		:	ed1son
+ *
+ *
+ *--------------------------------Revision History--------------------------------------
+ *	No	version		Data			Revised By			Item			Description
+ *
+ *
+ ***************************************************************************************/
+
+ /**************************************************************
+*	Multi-Include-Prevent Section
+**************************************************************/
+
+#ifndef DULINKEDLIST_H_INCLUDED
+#define DULINKEDLIST_H_INCLUDED
+
+/**************************************************************
+*	Macro Define Section
+**************************************************************/
+
+#define OVERFLOW -1
+
+/**************************************************************
+*	Struct Define Section
+**************************************************************/
+
+// define element type
+typedef int ElemType;
+
+// define struct of linked list
+typedef struct DuLNode {
+	ElemType data;
+	struct DuLNode *prior, *next;
+} DuLNode, *DuLinkedList;
+
+// define status
+typedef enum Status {
+	ERROR,
+	SUCCESS,
+} Status;
+
+
+/**************************************************************
+*	Prototype Declare Section
+**************************************************************/
+
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status InitList_DuL(DuLinkedList *L);
+
+/**
+ *  @name        : void DestroyList_DuL(DuLinkedList *L)
+ *	@description : destroy a linked list
+ *	@param		 : L(the head node)
+ *	@return		 : status
+ *  @notice      : None
+ */
+void DestroyList_DuL(DuLinkedList *L);
+/**
+ *  @name        : Status InsertBeforeList_DuL(DuLNode *p, LNode *q)
+ *	@description : insert node q before node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status InsertBeforeList_DuL(DuLNode *p, DuLNode *q);
+
+/**
+ *  @name        : Status InsertAfterList_DuL(DuLNode *p, DuLNode *q)
+ *	@description : insert node q after node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status InsertAfterList_DuL(DuLNode *p, DuLNode *q);
+
+/**
+ *  @name        : Status DeleteList_DuL(DuLNode *p, ElemType *e)
+ *	@description : delete the first node after the node p and assign its value to e
+ *	@param		 : p, e
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status DeleteList_DuL(DuLNode *p, ElemType *e);
+
+/**
+ *  @name        : void TraverseList_DuL(DuLinkedList L, void (*visit)(ElemType e))
+ *	@description : traverse the linked list and call the funtion visit
+ *	@param		 : L(the head node), visit
+ *	@return		 : Status
+ *  @notice      : None
+ */
+void TraverseList_DuL(DuLinkedList L, void(*visit)(ElemType e));
+
+
+//建立双链表
+void create(DuLNode *cre, int *arr, int n);
+
+//建立打印函数
+void print(ElemType e);
+
+//插入
+void insert(DuLNode *list);
+
+//找到最后一个节点
+DuLNode *final(DuLNode *find);
+
+//菜单
+void menu();
+
+//封装删除函数
+void delete(DuLNode *del);
+
+//封装更新函数
+void update(DuLNode *ln);
+/**************************************************************
+*	End-Multi-Include-Prevent Section
+**************************************************************/
+#endif
